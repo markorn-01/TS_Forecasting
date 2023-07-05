571b79a37e4b7f49fc9df5205f48cf1eb7e13b37
Minh <minh17301@gmail.com>
2023-07-05 19:43:18 +0700

    add split_data(), normalize_data() and WindowGenerator class

--- source/data_process.py
+++ source/data_process.py
@@ -11,26 +11,107 @@ import tensorflow as tf
 
 def get_data(csv_path):
     #this path is used for testing only. Otherwise, use the path in the main function.
-    csv_path = "data/jena_climate/jena_climate_2009_2016.csv"
+    csv_path = "data/gold/LBMA-GOLD.csv"
     df = pd.read_csv(csv_path) 
-    df = df[5::6]
-    date_time = pd.to_datetime(df.pop('Date Time'), format='%d.%m.%Y %H:%M:%S')
-    return df, date_time    
+    date_time = pd.to_datetime(df.pop('Date'))
+    df[df.isna()] = 0.00
+    return df , date_time    
 
-def clean_data(df):
-    #Convert error values to 0.0
-    df[df['wv (m/s)']==-9999.0] = 0.0
-    df[df['max. wv (m/s)']==-9999.0] = 0.0
-    #extract wind velocity
-    wv = df.pop('wv (m/s)')
-    max_wv = df.pop('max. wv (m/s)')
-    # Convert to radians.
-    wd_rad = df.pop('wd (deg)')*np.pi / 180
-    # Calculate the wind x and y components.
-    df['Wx'] = wv*np.cos(wd_rad)
-    df['Wy'] = wv*np.sin(wd_rad)
-    # Calculate the max wind x and y components.
-    df['max Wx'] = max_wv*np.cos(wd_rad)
-    df['max Wy'] = max_wv*np.sin(wd_rad)
-    return df
+def split_data(df):
+    #split dataset into train, validation, and test (80%, 10%, 10%)
+    column_indices = {name: i for i, name in enumerate(df.columns)}
+    n = len(df)
+    train_df = df[0: int(n*0.8)]
+    val_df = df[int(n*0.8): int(n*0.9)]
+    test_df = df[int(n*0.9):]
+    num_features = df.shape[1]
+    return train_df, val_df, test_df, num_features
 
+def normalize_data(train_df, val_df, test_df):
+    #normalize the data
+    train_mean = train_df.mean()
+    train_std = train_df.std()
+    train_df = (train_df - train_mean) / train_std
+    val_df = (val_df - train_mean) / train_std
+    test_df = (test_df - train_mean) / train_std
+    return train_df, val_df, test_df
+ 
+class WindowGenerator():
+    def __init__(self, 
+                input_width, 
+                label_width, 
+                shift,
+                train_df, 
+                val_df, 
+                test_df,
+                label_columns=None):
+        # Store the raw data.
+        self.train_df = train_df
+        self.val_df = val_df
+        self.test_df = test_df
+
+        # Work out the label column indices.
+        self.label_columns = label_columns
+        if label_columns is not None:
+            self.label_columns_indices = {name: i for i, name in
+                                          enumerate(label_columns)}
+        self.column_indices = {name: i for i, name in
+                            enumerate(train_df.columns)}
+
+        # Work out the window parameters.
+        self.input_width = input_width
+        self.label_width = label_width
+        self.shift = shift
+
+        self.total_window_size = input_width + shift
+
+        self.input_slice = slice(0, input_width)
+        self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+        self.label_start = self.total_window_size - self.label_width
+        self.labels_slice = slice(self.label_start, None)
+        self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+    def split_window(self, features):
+        inputs = features[:, self.input_slice, :]
+        labels = features[:, self.labels_slice, :]
+        if self.label_columns is not None:
+            labels = tf.stack(
+                [labels[:, :, self.column_indices[name]] for name in self.label_columns],
+                axis=-1)
+
+        # Slicing doesn't preserve static shape information, so set the shapes
+        # manually. This way the `tf.data.Datasets` are easier to inspect.
+        inputs.set_shape([None, self.input_width, None])
+        labels.set_shape([None, self.label_width, None])
+        return inputs, labels
+
+    def __repr__(self):
+        return '\n'.join([
+            f'Total window size: {self.total_window_size}',
+            f'Input indices: {self.input_indices}',
+            f'Label indices: {self.label_indices}',
+            f'Label column name(s): {self.label_columns}'])
+        
+df, datetime = get_data("data/gold/LBMA-GOLD.csv")
+train_df, val_df, test_df, num_features = split_data(df)
+train_df, val_df, test_df = normalize_data(train_df, val_df, test_df)
+w2 = WindowGenerator(input_width=6, 
+                     label_width=1, 
+                     shift=1,
+                     train_df=train_df,
+                     val_df=val_df,
+                     test_df=test_df,
+                     label_columns=['USD (AM)'])
+
+# Stack three slices, the length of the total window.
+example_window = tf.stack([np.array(train_df[:w2.total_window_size]),
+                           np.array(train_df[100:100+w2.total_window_size]),
+                           np.array(train_df[200:200+w2.total_window_size])])
+
+example_inputs, example_labels = w2.split_window(example_window)
+
+print('All shapes are: (batch, time, features)')
+print(f'Window shape: {example_window.shape}')
+print(f'Inputs shape: {example_inputs.shape}')
+print(f'Labels shape: {example_labels.shape}')
